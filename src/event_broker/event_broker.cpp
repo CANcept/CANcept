6e7741c416d205066295ad53344c816e0dcf4e34
nelespatzier <nele.spatzier@gmail.com>
2026-01-23 12:03:33 +0100

    Reversed event_broker.cpp merge conflict

--- src/event_broker/event_broker.cpp
+++ src/event_broker/event_broker.cpp
@@ -2,22 +2,37 @@
 
 namespace EventBroker {
 
-// Stub _publish method
-void EventBroker::_publish(const std::type_index type, const void* data)
+void EventBroker::_publish(std::type_index type, const void* data) 
 {
-    // do nothing for now
-    (void)type;
-    (void)data;
+    auto &channel = getChannel(type);
+    channel.trigger(data);
 }
 
-// Stub _subscribe method
-auto EventBroker::_subscribe(const std::type_index type,
-                             const std::function<void(const void*)> callback) -> Core::Connection
-{
-    // do nothing for now
-    (void)type;
-    (void)callback;
-    return Core::Connection{};  // return default empty connection
-}
 
+auto EventBroker::_subscribe(std::type_index type,
+                            std::function<void(const void*)> callback)
+    -> Core::Connection
+{
+    auto &channel = getChannel(type);
+
+    // Wrap std::function into a callable object
+    struct Wrapper {
+        std::function<void(const void*)> cb;
+
+        void operator()(const void *& data) {
+            cb(data);
+        }
+    };
+
+    auto wrapper = std::make_shared<Wrapper>(Wrapper{std::move(callback)});
+
+    entt::connection conn =
+        channel.sink().connect<&Wrapper::operator()>(*wrapper);
+
+    return Core::Connection(
+        [c = std::move(conn), w = std::move(wrapper)]() mutable {
+            c.release();
+        }
+    );
+}
 }  // namespace EventBroker
