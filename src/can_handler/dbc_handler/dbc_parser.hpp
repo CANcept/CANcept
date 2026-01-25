564494380e58c15c5108469265ce2b2b47f802f4
Florian <florian@fehrle.ent>
2026-01-25 13:01:08 +0100

    CBS-95 fix: added documentation to dbc parser

--- src/can_handler/dbc_handler/dbc_parser.hpp
+++ src/can_handler/dbc_handler/dbc_parser.hpp
@@ -29,38 +29,38 @@ class DbcParser
    private:
     /**
      * @brief tries to parse the start of the provided file to a signal (SG_:)
-     * @return The parsed signal or a null pointer if no signal was parsed
+     * @return The parsed signal
      */
     auto parseSignal() -> Core::DbcSignalDescription;
     /**
      * @brief tries to parse the start of the provided file to a message (BO_:)
-     * @return The parsed message or a null pointer if no message was parsed
+     * @return The parsed message
      */
     auto parseMessage() -> Core::DbcMessageDescription;
     /**
      * @brief tries to parse the start of the provided file to a value description
-     * @return The parsed value description or a null pointer if no value description was parsed
+     * @return The parsed value description
      */
     auto parseValueDescription() -> Core::DbcValueDescription;
     /**
      * @brief tries to parse the start of the provided file to a signal value description (VAL_:)
-     * @return The parsed signal value description or a null pointer if no signal value description
+     * @return The parsed signal value description
      * was parsed
      */
     auto parseSignalValue() -> Core::DbcSignalValueDescription;
     /**
      * @brief tries to parse the start of the provided file to a list of nodes (BU_:)
-     * @return The parsed list of nodes or a null pointer if no list of nodes was parsed
+     * @return The parsed list of nodes
      */
     auto parseNodes() -> std::list<std::string>;
     /**
      * @brief tries to parse the start of the provided file to a comment (CM_:)
-     * @return The parsed comment or a null pointer if no comment was parsed
+     * @return The parsed comment
      */
     auto parseComment() -> std::string;
     /**
      * @brief tries to parse the start of the provided file to a version (Version:)
-     * @return The parsed version or a null pointer if no version was parsed
+     * @return The parsed version
      */
     auto parseVersion() -> std::string;
     /**
@@ -69,7 +69,7 @@ class DbcParser
     void parseNewSymbols();
     /**
      * @brief Parses a value table (VAL_TAB_)
-     * @return A bool indicating if a value tabel has been parsed
+     * @return A bool indicating if a value table has been parsed
      */
     auto parseValueTable() -> bool;
     /**
@@ -80,22 +80,84 @@ class DbcParser
      * @brief Parses a environment variable (EV_)
      */
     auto parseEnvironmentVariable() -> bool;
+    /**
+     * @brief parses environment variable data (ENVVAR_DATA_)
+     */
     void parseEnvironmentVariableData();
+    /**
+     * @brief Parses environment variable value definitions (VAL_)
+     */
     void parseEnvironmentVariableValueDefinitions();
+    /**
+     * @brief Parses a signal type (SGTYPE_)
+     * @return A bool indicating if a signal type has been parsed
+     */
     auto parseSignalType() -> bool;
+    /**
+     * @brief Parses a signal type reference (SGTYPE_)
+     * @return A bool indicating if a signal type reference has been parsed
+     */
     auto parseSignalTypeReference() -> bool;
+    /**
+     * @brief Parses a signal group  (SIG_GROUP_)
+     */
     void parseSignalGroup();
+    /**
+     * @brief Parses an attribute definition (BA_DEF_)
+     * @return
+     */
     auto parseAttributeDefinition() -> bool;
+    /**
+     * @brief Parses an attribute value (BA_)
+     * @returnA bool indicating if an attribute value has been parsed
+     */
     auto parseAttributeValue() -> bool;
+    /**
+     * @brief Parses an attribute default value (BA_DEF_DEF_)
+     * @return A bool indicating if an attribute default value has been parsed
+     */
     auto parseAttributeDefault() -> bool;
+    /**
+     * @brief Parses a signal extended value type list (SIG_VALTYPE_)
+     */
     void parseSignalExtendedValueTypeList();
+    /**
+     * @brief Parses the bit timing (BS_:)
+     */
     void parseBitTiming();
+    /**
+     * @brief Removes all spaces at the front of the current @ref CanHandler::file
+     */
     void eraseSpaces();
+    /**
+     * @brief Parses a CIdentifier (testIdentifier dbcFileRest)
+     * @return A string containing the CIdentifier (testIdentifier)
+     */
     auto parseCIdentifier() -> std::string;
+    /**
+     * @brief Parses a String ("exampleString" dbcFileRest)
+     * @return A string containing the string (exampleString)
+     */
     auto parseString() -> std::string;
+    /**
+     * @brief Parses an unsigned int
+     * @return The unsigned int
+     */
     auto parseUInt() -> uint;
+    /**
+     * Parses a signed int
+     * @return The signed int
+     */
     auto parseInt() -> int;
+    /**
+     * Parses a double
+     * @return The double
+     */
     auto parseDouble() -> double;
+    /**
+     * Truncates the current @ref file to the next semicolon
+     * @return A bool indicating there was a semicolon to truncate to
+     */
     auto truncateToNextSemicolon() -> bool;
     /**
      * @brief The current file to parse, truncated to the point of the current parse
@@ -109,6 +171,9 @@ class DbcParser
      * @brief Flag indicating if the last called function parsed an object
      */
     bool parsedObject = false;
+    /**
+     * @brief Mutex, that protects the file from being overwritten in the parsing process
+     */
     std::mutex fileMutex;
     const std::pmr::set<std::string> symbols{"CM_",
                                              "BA_DEF_",
