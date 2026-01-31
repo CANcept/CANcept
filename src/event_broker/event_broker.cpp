96e0251642a3a741edc2d3284e727febe9e3533e
Florian <florian@fehrle.net>
2026-01-31 22:04:34 +0100

    CBS-82 fix: clang format issues

--- src/event_broker/event_broker.cpp
+++ src/event_broker/event_broker.cpp
@@ -2,33 +2,30 @@
 namespace EventBroker {
 
 auto EventBroker::_subscribe(std::type_index type,
-                            std::function<void(const void*)> callback)
-    -> Core::Connection
+                             std::function<void(const void*)> callback) -> Core::Connection
 {
-    auto &channel = getChannel(type);
+    auto& channel = getChannel(type);
 
     // Wrap std::function into a callable object
     struct Wrapper {
         std::function<void(const void*)> callback;
 
-        void operator()(const void *& data) {
+        void operator()(const void*& data)
+        {
             callback(data);
         }
     };
 
     auto wrapper = std::make_shared<Wrapper>(Wrapper{std::move(callback)});
 
-    entt::connection connection =
-        channel.sink().connect<&Wrapper::operator()>(*wrapper);
+    entt::connection connection = channel.sink().connect<&Wrapper::operator()>(*wrapper);
 
-    return Core::Connection(
-        [connection = std::move(connection), wrapper = std::move(wrapper)]() mutable {
-            connection.release();
-        }
-    );
+    return Core::Connection([connection = std::move(connection),
+                             wrapper = std::move(wrapper)]() mutable { connection.release(); });
 }
 
-void EventBroker::_publish(std::type_index type, const void* data) {
+void EventBroker::_publish(std::type_index type, const void* data)
+{
     getChannel(type).trigger(data);
 }
-} // namespace EventBroker
+}  // namespace EventBroker
