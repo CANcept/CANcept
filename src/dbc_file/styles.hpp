07875fae95abb8989595fc98d466e7c7c4e0eae4
[udgjo] <udgjo@student.kit.edu>
2026-02-14 01:56:08 +0100

    CBS-117: outsourced all style sheets in dbc_file to new file styles.cpp

---
+++ src/dbc_file/styles.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QString>
+
+class QWidget;
+
+namespace DbcFile::Style
+{
+namespace LoadPage{
+QString uploadZone();
+QString uploadInstruction();
+QString statusLabel(bool isError);
+}
+namespace OverviewPage
+{
+QString scrollArea();
+QString secondaryLabel();
+QString statTitle();
+QString statValue();
+}
+namespace EcusPage
+{
+QString treeStyle();
+}
+namespace MessagesPage
+{
+QString detailList();
+QString detailLabel();
+}
+}
