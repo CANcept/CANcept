752a7d89e3e500b63e02c403e14fcd5723ad7dbf
[udgjo] <udgjo@student.kit.edu>
2026-03-10 09:45:17 +0100

    CBS-129: dbc_utils path fix

--- tests/unit/core/util/test_dbc_utils.cpp
+++ tests/unit/core/util/test_dbc_utils.cpp
@@ -6,7 +6,6 @@
 #include "core/util/dbc_utils.hpp"
 #include "dbc_file/constants.hpp"
 
-using namespace DbcFile::Util;
 inline void PrintTo(const QString& qString, std::ostream* os)
 {
     *os << "\"" << qString.toStdString() << "\"";
@@ -19,38 +18,38 @@ inline void PrintTo(const QString& qString, std::ostream* os)
 TEST(DbcUtilsTest, FormatIdReturnsHexUpperCase)
 {
     // Case 1: Standard ID (decimal 255 -> hex FF)
-    EXPECT_EQ(DbcFile::Util::formatId(255), "0x0FF");
+    EXPECT_EQ(Core::formatId(255), "0x0FF");
 
     // Case 2: Zero padding (decimal 10 -> hex A -> 00A)
-    EXPECT_EQ(formatId(10), "0x00A");
+    EXPECT_EQ(Core::formatId(10), "0x00A");
 
     // Case 3: Zero
-    EXPECT_EQ(formatId(0), "0x000");
+    EXPECT_EQ(Core::formatId(0), "0x000");
 
     // Case 4: Larger values (decimal 4095 -> hex FFF)
-    EXPECT_EQ(formatId(4095), "0xFFF");
+    EXPECT_EQ(Core::formatId(4095), "0xFFF");
 
     // Case 5: Ensure uppercase
-    EXPECT_EQ(formatId(2748), "0xABC");  // 2748 dec = ABC hex
+    EXPECT_EQ(Core::formatId(2748), "0xABC");  // 2748 dec = ABC hex
 }
 
 TEST(DbcUtilsTest, FormatNumberRemovesTrailingZeros)
 {
     // Case 1: Integer as double
-    EXPECT_EQ(formatNumber(40.0), "40");
+    EXPECT_EQ(Core::formatNumber(40.0), "40");
 
     // Case 2: Exact decimal
-    EXPECT_EQ(formatNumber(0.125), "0.125");
+    EXPECT_EQ(Core::formatNumber(0.125), "0.125");
 
     // Case 3: Negative number
-    EXPECT_EQ(formatNumber(-5.5), "-5.5");
+    EXPECT_EQ(Core::formatNumber(-5.5), "-5.5");
 
     // Case 4: Zero
-    EXPECT_EQ(formatNumber(0.0), "0");
+    EXPECT_EQ(Core::formatNumber(0.0), "0");
 }
 
 TEST(DbcUtilsTest, FormatRangeReturnsBracketString)
 {
-    EXPECT_EQ(formatRange(0, 100), "[0, 100]");
-    EXPECT_EQ(formatRange(5, 5), "[5, 5]");
+    EXPECT_EQ(Core::formatRange(0, 100), "[0, 100]");
+    EXPECT_EQ(Core::formatRange(5, 5), "[5, 5]");
 }
